9e1a876f79665e2ba6c7f25160bdd8c88fddebe8
b44ken <bradboratto@gmail.com>
2020-05-30 16:31:14 -0400

    fix 'scrapers.includes is not a function'

--- src/main.js
+++ src/main.js
@@ -6,23 +6,16 @@
   scraper.style.display = "none"
   scraper.style.position = "sticky"
   scraper.style.overflow = "auto";
-  scraper.id = "box"
 
   document.body.appendChild(scraper)
-  
-	document.body.addEventListener("mouseout", event => {
-    scraper.style.display = "none";
-  })
 
   document.body.addEventListener("mouseover", event => {
+	scraper.style.display = "none"
     const onlyLinks = event.path.filter(e => e.nodeName == "A");
     if (onlyLinks.length == 0) return false;
     const url = new URL(onlyLinks[0]);
-    const hostname = new URL(onlyLinks[0]).hostname;
-    document.getElementById("box").innerHTML= "brad's problem lul";
-    if (scrapers.includes(hostname)) {
-    scraper.style.display = "block";
-    } else {
-    return false;
-    }
+	const hostname = new URL(onlyLinks[0]).hostname;
+    if (!scrapers[hostname]) return false
+	scraper.style.display = "block";
+	scraper.innerHTML = "brad's problem lul";
   })
