2c644a2aa2895a09edca9fe9ee4a61f2bcafe043
b44ken <bradboratto@gmail.com>
2020-05-30 17:10:59 -0400

    functional youtube preview

--- src/main.js
+++ src/main.js
@@ -1,21 +1,35 @@
-  var scraper = document.createElement("div")
-  scraper.style.width = "300px"
-  scraper.style.height = "200px"
-  scraper.style.border = "3px solid #333"
-  scraper.style.borderRadius = "6px"
+var scraper = document.createElement("div")
+scraper.style.width = "300px"
+scraper.style.height = "200px"
+scraper.style.border = "3px solid #333"
+scraper.style.borderRadius = "6px"
+scraper.style.display = "none"
+scraper.style.position = "sticky"
+scraper.style.overflow = "auto";
+scraper.style.top = "50px";
+scraper.style.left = "50px";
+
+document.body.appendChild(scraper)
+
+document.body.addEventListener("mouseover", event => {
   scraper.style.display = "none"
-  scraper.style.position = "sticky"
-  scraper.style.overflow = "auto";
 
-  document.body.appendChild(scraper)
+  const onlyLinks = event.path.filter(e => e.nodeName == "A");
+  if (onlyLinks.length == 0) return false;
+  
+  const url = new URL(onlyLinks[0]);
+  const hostname = new URL(onlyLinks[0]).hostname;
+  if (!scrapers[hostname]) return false
 
-  document.body.addEventListener("mouseover", event => {
-	scraper.style.display = "none"
-    const onlyLinks = event.path.filter(e => e.nodeName == "A");
-    if (onlyLinks.length == 0) return false;
-    const url = new URL(onlyLinks[0]);
-	const hostname = new URL(onlyLinks[0]).hostname;
-    if (!scrapers[hostname]) return false
-	scraper.style.display = "block";
-	scraper.innerHTML = "brad's problem lul";
+  scraper.style.display = "block";
+  
+  fetch("https://api.allorigins.win/get?url=" + url.href)
+	  .then(e => e.json())
+	  .then(html => {
+		  console.log(html)
+		  dom = new DOMParser().parseFromString(html.contents, "text/html")
+		  dom.origin = url
+		  code = scrapers[hostname](dom)
+		  scraper.innerHTML = code
   })
+})
--- src/scrapers.js
+++ src/scrapers.js
@@ -1,15 +1,18 @@
 /* individual scrapers */
 const scrapers = {}
 
-scrapers["www.amazon.com"] = html => {
+scrapers["www.amazon.com"] = dom => {
+	console.log(dom.querySelector("#priceblock_ourprice"))
 	const price = dom.querySelector("#priceblock_ourprice").innerText
 	const name = dom.querySelector("#productTitle").textContent.substr(161).split("\n")[0]
 	const rating = dom.querySelector("#acrPopover").title
 	const image = dom.querySelector("#landingImage").src
 	
-	return buildShop({price, name, rating, image})
+	return JSON.stringify({price, name, rating, image})
 }
 
-function buildShop(obj) {
-	return JSON.stringify(obj)
+scrapers["www.youtube.com"] = dom => {
+	dom.origin = "https://www.youtube.com/watch?v=0SyLqFuf_SU"
+	videoID = dom.orign.substr(32)
+	return `<iframe src="https://youtube.com/embed/${videoID}"></iframe>`
 }
