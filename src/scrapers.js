883e5d70fe52d4f181dbdb451aca8e62f8634e0d
whatYouEgg-StabsHim <60416777+whatYouEgg-StabsHim@users.noreply.github.com>
2020-05-31 11:50:32 -0400

    Update scrapers.js

--- src/scrapers.js
+++ src/scrapers.js
@@ -16,3 +16,16 @@ scrapers["www.youtube.com"] = dom => {
 	const videoID = origin.substr(32)
 	return `<iframe src="https://youtube.com/embed/${videoID}"></iframe>`
 }
+scrapers["en.wikipedia.org"] = dom => {
+    const title = dom.querySelector("#firstHeading").innerText
+    const contents = dom.querySelector("#toc").innerText.replace(/(\r\n|\n|\r)/gm,"<br>")
+
+    return JSON.stringify({title,contents})
+}
+scrapers["github.com"] = dom => {
+    const title = dom.querySelector(".url.fn").innerText
+    const child = dom.querySelector(".mr-2.flex-self-stretch").innerHTML
+    const desc = dom.querySelector(".text-gray-dark.mr-2").innerText
+
+    return JSON.stringify({title,child,desc})
+}
