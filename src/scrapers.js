67b3ce60014862cb3d97db8c15111d9a366e3caf
whatYouEgg-StabsHim <60416777+whatYouEgg-StabsHim@users.noreply.github.com>
2020-05-31 12:45:52 -0400

    fixes homepage hovering problem

--- src/scrapers.js
+++ src/scrapers.js
@@ -2,19 +2,33 @@
 const scrapers = {}
 
 scrapers["www.youtube.com"] = (dom, url) => {
-	const videoID = url.split("watch?v=")[1]
-	return `<iframe src="https://youtube.com/embed/${videoID}/?autoplay=1"></iframe>`
+    const videoID = url.split("watch?v=")[1]
+
+    if (url == "youtube.com" || url.includes("search")) {
+    return false 
+    } else {
+    return `<iframe src="https://youtube.com/embed/${videoID}/?autoplay=1"></iframe>`
+    }	
+
 }
-scrapers["en.wikipedia.org"] = dom => {
+scrapers["en.wikipedia.org"] = (dom, url) => {
     const title = dom.querySelector("#firstHeading").innerText
     const contents = dom.querySelector("#toc").innerText.replace(/(\r\n|\n|\r)/gm,"<br>")
 
+    if (url == "en.wikipedia.org") { 
+    return false
+    } else {
     return JSON.stringify({title,contents})
+    }
 }
-scrapers["github.com"] = dom => {
+scrapers["github.com"] = (dom, url) => {
     const title = dom.querySelector(".url.fn").innerText
     const child = dom.querySelector(".mr-2.flex-self-stretch").innerHTML
     const desc = dom.querySelector(".text-gray-dark.mr-2").innerText
 
+    if (url == "https://github.com" || url.includes("search")) { 
+    return false
+    } else {
     return JSON.stringify({title,child,desc})
+    }
 }
